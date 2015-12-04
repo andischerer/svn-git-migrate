ec70788d9d62a894c244476f6ec6f84990fbe969
Andreas Scherer <t3asdev@googlemail.com>
2015-12-04 17:13:46 +0100

    add prefix as default clone parameter

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ migrate.clone = function (svnRepoUrl, options) {
 	];
 	gitArgs.push(svnRepoUrl);
 	gitArgs.push('--stdlayout');
+	gitArgs.push('--prefix=origin/');
 
 	return execCommand(gitArgs.concat(parseOptions(options))).then(() => Promise.resolve(repoName));
 };
